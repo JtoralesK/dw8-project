51632654395f5d3aa283845aa2068924be3ebaeb
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2022-10-27 21:55:07 -0300

    cambiando la fuente de la pagina a poppins

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -28,10 +28,8 @@ import {obtieneUser} from"../../hooks/hooks"
             <h1 className={css.titulo}>¡Encontrá a tu mejor amigo en pocas horas!</h1>
             <p className={css.parrafo}>Vamos a ayudarte a viralizar la imagen de tu mascota perdida. Logramos rastreos
                inteligentes a través de publicidad geolocalizada en redes sociales.
-               <br></br><br></br>
                 Aseguramos llegar a tus vecinos y a miles de personas de la zona donde se
                extravió tu mejor amigo.
-               <br></br><br></br>
               No importa si fue robado, se escapó o simplemente se perdió, ¡podemos encontrarlo</p>
            </div>
        <div className={css.contenedorImg}>
